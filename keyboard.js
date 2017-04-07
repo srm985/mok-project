a3f30417bd23eb0c669eec0cc82f86755a1b4c30
Sean M <srmcquay@gmail.com>
2017-04-06 20:22:55 -0500

    Working Save

--- keyboard.js
+++ keyboard.js
@@ -35,31 +35,62 @@
         rowData_4[0] = keyObject[48];
         rowData_4 = rowData_4.concat(keyObject.slice(37, 47));
 
+        $('body').append('<div class="keyboard-wrapper"></div>');
+
         generateRow(rowData_1);
         generateRow(rowData_2);
         generateRow(rowData_3);
         generateRow(rowData_4);
+
+        keyboardFillout();
+        sizeKeys();
     }
 
     function appendKey(keyCode) {
         if (keyCode.length == 4) {
-            $('.keyboard-row:last').append('<div class="keyboard-key">&#x' + keyCode + '</div>');
+            $('.keyboard-row:last').append('<button class="keyboard-key keyboard-key-sm">&#x' + keyCode + '</button>');
         } else {
-            $('.keyboard-row:last').append('<div class="keyboard-key">' + keyCode + '</div>');
+            $('.keyboard-row:last').append('<button class="keyboard-key keyboard-key-sm">' + keyCode + '</button>');
         }
     }
 
     function generateRow(keyListSplit) {
-        $('body').append('<div class="keyboard-row"></div>');
+        $('.keyboard-wrapper').append('<div class="keyboard-row"></div>');
         $.each(keyListSplit, function(i, value) {
             appendKey(value[3]);
         });
     }
 
+    function keyboardFillout() {
+        $('.keyboard-row:eq(0)').append('<button class="keyboard-key keyboard-key-lg">Backspace</button>');
+        $('.keyboard-row:eq(1)').prepend('<button class="keyboard-key keyboard-key-lg">Tab</button>');
+        $('.keyboard-row:eq(2)').prepend('<button class="keyboard-key keyboard-key-lg">Caps Lock</button>');
+        $('.keyboard-row:eq(2)').append('<button class="keyboard-key keyboard-key-lg">Enter</button>');
+        $('.keyboard-row:eq(3)').prepend('<button class="keyboard-key keyboard-key-lg">Shift</button>');
+        $('.keyboard-row:eq(3)').append('<button class="keyboard-key keyboard-key-lg">Shift</button>');
+    }
+
+    function sizeKeys() {
+        var rowWidth = $('.keyboard-row').width(),
+            keyPadding = 2 * ($('.keyboard-key').css('padding-right')).match(/[0-9]/),
+            smallKeys, largeKeys, smallKeyConstant = rowWidth / 40,
+            largeKeyWidth;
+        $('.keyboard-row').each(function() {
+            smallKeys = $(this).children('.keyboard-key-sm').length;
+            largeKeys = $(this).children('.keyboard-key-lg').length;
+            largeKeyWidth = (rowWidth - ((smallKeys + largeKeys) * keyPadding) - (smallKeys * smallKeyConstant)) / largeKeys;
+            console.log((rowWidth - ((smallKeys + largeKeys) * keyPadding) - (smallKeys * smallKeyConstant)));
+            console.log(smallKeys + largeKeys);
+            $('.keyboard-key-sm').width(smallKeyConstant);
+            $('.keyboard-key-lg').width(largeKeyWidth);
+        });
+    }
+
     var file;
+
     function languageSwap() {
         $('.keyboard-row').remove();
-        if(file == 'albanian.klc') {
+        if (file == 'albanian.klc') {
             file = 'belarusian.klc';
         } else {
             file = 'albanian.klc';
