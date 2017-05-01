a409785780c236e4dc4e0450dabe15b9fe4c01a5
Sean McQuay <smcquay@securustechnologies.com>
2017-05-01 08:08:05 -0500

    Release V1.0

--- keyboard.js
+++ keyboard.js
@@ -7,7 +7,7 @@
 //*            GitHub: https://github.com/srm985/mok-project                        *
 //*                                                                                 *
 //*            Started: March 2017                                                  *
-//*            Version: 1.0                                                           *
+//*            Version: 1.0                                                         *
 //*                                                                                 *
 //*            License: MIT (https://opensource.org/licenses/MIT)                   *
 //*                                                                                 *
